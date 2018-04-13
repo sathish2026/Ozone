fac167c86c6a5fd2e5a5a34555b5af1dc50f0760
sathish2026 <txt2sathish@gmail.com>
2018-04-13 17:14:53 +0530

    ozone: configure travis

---
+++ push_pass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
+FILENAME=Ozone-$(date +"%Y%m%d-%H:%M").apk
+COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty="%h")
+MSG="Build <a href=%27https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}%27>%23${TRAVIS_BUILD_NUMBER}</a> (<a href=%27https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}%27>${COMMIT}</a>) of ${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH} by ${AUTHOR_NAME} passed."
+mv app/build/outputs/apk/debug/*.apk ${FILENAME}
+CHANGELOG="Changelog:
+$(git log ${TRAVIS_COMMIT_RANGE} --pretty=format:'%h: %s by %aN%n')"
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -F chat_id=${CHAT_ID} -F document=@${FILENAME}
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=$CHANGELOG&parse_mode=HTML"
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=$MSG&parse_mode=HTML"
